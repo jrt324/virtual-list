12b76921caa8e405197c28dc93e4b127bd583193
jrt324 <jrt324@gmail.com>
2013-09-10 17:08:19 +0800

    Add support to IE 6-8

--- vlist.js
+++ vlist.js
@@ -4,7 +4,7 @@
  * Copyright (C) 2013 Sergi Mansilla
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the 'Software'), to deal
+ * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
@@ -13,7 +13,7 @@
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
  * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -31,102 +31,78 @@
  */
 function VirtualList(config) {
   var width = (config && config.w + 'px') || '100%';
-  var height = (config && config.h + 'px') || '100%';
+  var height = this.height = (config && config.h + 'px') || '100%';
   var itemHeight = this.itemHeight = config.itemHeight;
 
   this.items = config.items;
   this.generatorFn = config.generatorFn;
   this.totalRows = config.totalRows || (config.items && config.items.length);
 
-  var scroller = VirtualList.createScroller(itemHeight * this.totalRows);
+  var totalHeight = itemHeight * this.totalRows;
+  this.scroller = VirtualList.createScroller(totalHeight);
   this.container = VirtualList.createContainer(width, height);
-  this.container.appendChild(scroller);
 
   var screenItemsLen = Math.ceil(config.h / itemHeight);
   // Cache 4 times the number of items that fit in the container viewport
-  this.cachedItemsLen = screenItemsLen * 3;
-  this._renderChunk(this.container, 0);
+  var cachedItemsLen = screenItemsLen * 3;
+  this._renderChunk(this.container, 0, cachedItemsLen / 2);
 
   var self = this;
   var lastRepaintY;
   var maxBuffer = screenItemsLen * itemHeight;
-  var lastScrolled = 0;
-
-  // As soon as scrolling has stopped, this interval asynchronouslyremoves all
-  // the nodes that are not used anymore
-  this.rmNodeInterval = setInterval(function() {
-    if (Date.now() - lastScrolled > 100) {
-      var badNodes = document.querySelectorAll('[data-rm="1"]');
-      for (var i = 0, l = badNodes.length; i < l; i++) {
-        self.container.removeChild(badNodes[i]);
-      }
-    }
-  }, 200);
 
   function onScroll(e) {
     var scrollTop = e.target.scrollTop;
     var first = parseInt(scrollTop / itemHeight) - screenItemsLen;
     first = first < 0 ? 0 : first;
     if (!lastRepaintY || Math.abs(scrollTop - lastRepaintY) > maxBuffer) {
-      self._renderChunk(self.container, first);
+      self._renderChunk(self.container, first, cachedItemsLen);
       lastRepaintY = scrollTop;
     }
 
-    lastScrolled = Date.now();
     e.preventDefault && e.preventDefault();
   }
 
   this.container.addEventListener('scroll', onScroll);
 }
 
-VirtualList.prototype.createRow = function(i) {
-  var item;
-  if (this.generatorFn)
-    item = this.generatorFn(i);
-  else if (this.items) {
-    if (typeof this.items[i] === 'string') {
-      var itemText = document.createTextNode(this.items[i]);
-      item = document.createElement('div');
-      item.style.height = this.itemHeight + 'px';
-      item.appendChild(itemText);
-    } else {
-      item = this.items[i];
-    }
-  }
-
-  item.classList.add('vrow');
-  item.style.position = 'absolute';
-  item.style.top = (i * this.itemHeight) + 'px';
-  return item;
-};
-
 /**
- * Renders a particular, consecutive chunk of the total rows in the list. To
- * keep acceleration while scrolling, we mark the nodes that are candidate for
- * deletion instead of deleting them right away, which would suddenly stop the
- * acceleration. We delete them once scrolling has finished.
- *
- * @param {Node} node Parent node where we want to append the children chunk.
- * @param {Number} from Starting position, i.e. first children index.
+ * Renders a particular, consecutive chunk of the total rows in the list.
+ * @param {Node} node
+ * @param {Number} fromPos
+ * @param {Number} howMany
  * @return {void}
  */
-VirtualList.prototype._renderChunk = function(node, from) {
-  var finalItem = from + this.cachedItemsLen;
+VirtualList.prototype._renderChunk = function(node, fromPos, howMany) {
+  var fragment = document.createDocumentFragment();
+  fragment.appendChild(this.scroller);
+
+  var finalItem = fromPos + howMany;
   if (finalItem > this.totalRows)
     finalItem = this.totalRows;
 
-  // Append all the new rows in a document fragment that we will later append to
-  // the parent node
-  var fragment = document.createDocumentFragment();
-  for (var i = from; i < finalItem; i++) {
-    fragment.appendChild(this.createRow(i));
+  for (var i = fromPos; i < finalItem; i++) {
+    var item;
+    if (this.generatorFn)
+      item = this.generatorFn(i);
+    else {
+      if (typeof this.items[i] === 'string') {
+        var itemText = document.createTextNode(this.items[i]);
+        item = document.createElement('div');
+        item.style.height = this.height;
+        item.appendChild(itemText);
+      } else {
+        item = this.items[i];
+      }
+    }
+	item.className += " vrow";
+    //item.classList.add('vrow');
+    item.style.position = 'absolute';
+    item.style.top = (i * this.itemHeight) + 'px';
+    fragment.appendChild(item);
   }
 
-  // Hide and mark obsolete nodes for deletion.
-  for (var j = 1, l = node.childNodes.length; j < l; j++) {
-    node.childNodes[j].style.display = 'none';
-    node.childNodes[j].setAttribute('data-rm', '1');
-  }
+  node.innerHTML = '';
   node.appendChild(fragment);
 };
 
@@ -151,3 +127,34 @@ VirtualList.createScroller = function(h) {
   scroller.style.height = h + 'px';
   return scroller;
 };
+
+//addEventListener polyfill 1.0 / Eirik Backer / MIT Licence
+(function(win, doc){
+	if(win.addEventListener)return;		//No need to polyfill
+ 
+	function docHijack(p){var old = doc[p];doc[p] = function(v){return addListen(old(v))}}
+	function addEvent(on, fn, self){
+		return (self = this).attachEvent('on' + on, function(e){
+			var e = e || win.event;
+			e.preventDefault  = e.preventDefault  || function(){e.returnValue = false}
+			e.stopPropagation = e.stopPropagation || function(){e.cancelBubble = true}
+			e.target = e.srcElement;
+			fn.call(self, e);
+		});
+	}
+	function addListen(obj, i){
+		if(i = obj.length)while(i--)obj[i].addEventListener = addEvent;
+		else obj.addEventListener = addEvent;
+		return obj;
+	}
+ 
+	addListen([doc, win]);
+	if('Element' in win)win.Element.prototype.addEventListener = addEvent;			//IE8
+	else{		//IE < 8
+		doc.attachEvent('onreadystatechange', function(){addListen(doc.all)});		//Make sure we also init at domReady
+		docHijack('getElementsByTagName');
+		docHijack('getElementById');
+		docHijack('createElement');
+		addListen(doc.all);	
+	}
+})(window, document);
